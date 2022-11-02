00ff7df4059fbc6b3ea30f0e36b1ddf24b67cfc2
clare <clare@duomai.com>
2022-11-02 17:48:31 +0800

    test(list-search): list-search 增加测试用例、完善jest测试环境

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,11 @@
 
 module.exports = {
   preset: 'ts-jest',
+  // 自动清除 Mock
   clearMocks: true,
-
+  // 开启覆盖率
   collectCoverage: true,
+  // 指定生成覆盖率报告文件存放位置
   coverageDirectory: 'coverage',
 
   coverageProvider: 'v8',
---
+++ packages/design/src/list-search/__tests__/index.test.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import ListSearch from '../';
+
+describe('<ListSearch />', () => {
+  it('placeholder', () => {
+    const placeholder = '请输入';
+    render(<ListSearch placeholder={placeholder} />);
+    expect(screen.getByPlaceholderText(placeholder)).toBeInTheDocument();
+  });
+
+  it('value is not controlled', async () => {
+    const enterKey = '111';
+    render(<ListSearch />);
+    const input = screen.getByRole('textbox');
+    // 注焦、输入
+    input.focus();
+    await userEvent.keyboard(enterKey);
+    // 校验value是否发生改变
+    expect(screen.getByDisplayValue(enterKey)).toBeEmptyDOMElement();
+  });
+
+  it('value is controlled', async () => {
+    const defaultValue = 'defaultValue';
+    const changeValue = 'changeValue';
+    const btnLabel = '改变';
+    const enterKey = '111';
+
+    const Dom = () => {
+      const [value, setValue] = useState(defaultValue);
+      return (
+        <>
+          <button onClick={() => setValue(changeValue)}>{btnLabel}</button>
+          <ListSearch value={value} />
+        </>
+      );
+    };
+    render(<Dom />);
+
+    // 校验开始、为默认值
+    expect(screen.getByDisplayValue(defaultValue)).toBeEmptyDOMElement();
+    // 获取btn 、触发click、改变value
+    const changeBtn = screen.getByRole('button', { name: btnLabel });
+    await userEvent.click(changeBtn);
+    // 校验value是否发生改变
+    expect(screen.getByDisplayValue(changeValue)).toBeEmptyDOMElement();
+
+    // 获取input
+    const input = screen.getByRole('textbox');
+    // 注焦、输入
+    input.focus();
+    await userEvent.keyboard(enterKey);
+    // 校验value 是否没有发生改变
+    expect(screen.getByDisplayValue(changeValue)).toBeEmptyDOMElement();
+  });
+});
