307269c0920587d92f0c3c80584de5dd40989121
小陈啊！！！ <chen.lingang@qq.com>
2022-11-15 13:57:09 +0800

    feat(mixmin.less lsitlayout): listLayout增加loading 、修改混入代码、增加loading

--- packages/business/src/component/list-layout/index.tsx
+++ packages/business/src/component/list-layout/index.tsx
@@ -1,14 +1,16 @@
 import type { HTMLAttributes } from 'react';
 import classnames from 'classnames';
+import { Spin } from 'antd';
 import styles from './styles.less';
 
 interface ListLayoutProps extends HTMLAttributes<HTMLDivElement> {
   headerImg?: string;
   children?: React.ReactNode;
+  loading?: boolean;
 }
 
 const ListLayout: React.FC<ListLayoutProps> = (props) => {
-  const { children, headerImg, className, ...divProps } = props;
+  const { children, headerImg, className, loading, ...divProps } = props;
 
   return (
     <div className={classnames(styles.listLayout, className)} {...divProps}>
@@ -17,7 +19,12 @@ const ListLayout: React.FC<ListLayoutProps> = (props) => {
           <img src={headerImg} className={styles.headerImg} />
         </div>
       )}
-      <div className={styles.listBox}>{children}</div>
+      <div className={styles.listBox}>
+        {children}
+        <div className={classnames(styles.spinBox, { [styles.rHidden]: !loading })}>
+          <Spin />
+        </div>
+      </div>
     </div>
   );
 };
